c4e84011b0b6296dee5fa4e5b1db8bee68c9832f
Kelvin Wu <wu.kelvin91@gmail.com>
2025-01-29 18:14:29 +0800

    chore: done tutorial

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import PocketBase from "pocketbase";
+import CreateNote from "./CreateNote";
 
 export default async function NotesPage() {
   const notes = await getNotes();
@@ -10,23 +11,24 @@ export default async function NotesPage() {
           return <Note key={note.id} note={note} />;
         })}
       </div>
+      <CreateNote />
     </div>
   );
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function Note({ note }: any) {
-    const { id, title, content, created } = note || {};
-    return (
-      <Link href={`/notes/${id}`}>
-        <div className="p-4 bg-gray-100 shadow-md rounded-lg hover:shadow-lg transition-shadow duration-300 my-5">
-          <h2 className="text-xl font-semibold text-gray-800">{title}</h2>
-          <h5 className="text-gray-700 text-sm truncate">{content}</h5>
-          <p className="text-gray-500 text-xs mt-2">{created}</p>
-        </div>
-      </Link>
-    );
-  }
+  const { id, title, content, created } = note || {};
+  return (
+    <Link href={`/notes/${id}`}>
+      <div className="p-4 bg-gray-100 shadow-md rounded-lg hover:shadow-lg transition-shadow duration-300 my-5">
+        <h2 className="text-xl font-semibold text-gray-800">{title}</h2>
+        <h5 className="text-gray-700 text-sm truncate">{content}</h5>
+        <p className="text-gray-500 text-xs mt-2">{created}</p>
+      </div>
+    </Link>
+  );
+}
 
 type NoteType = {
   id: string;
@@ -38,10 +40,10 @@ type NoteType = {
 const getNotes = async () => {
   const db = new PocketBase("http://127.0.0.1:8090");
   const data = await db.collection("notes").getList(1, 20);
-    // const res = await fetch(
-    //   "http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30",
-    //   { cache: "no-store" }
-    // );
-    // const data = await res.json();
+  // const res = await fetch(
+  //   "http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30",
+  //   { cache: "no-store" }
+  // );
+  // const data = await res.json();
   return data?.items;
 };
